31b90afe7b76d8cc1ab22443f1201f0390d7ad55
ductoanvo <toan.ducvo@icloud.com>
2022-04-26 14:42:30 +0700

    initial commit

---
+++ app/src/main/java/vn/edu/iuh/fit/se/android/dhktpm15ctt/nhom10/toannhat/LoginActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.iuh.fit.se.android.dhktpm15ctt.nhom10.toannhat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
